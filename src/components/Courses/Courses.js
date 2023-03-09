102173355498cff82e2e7375b7a0603847ceaf6b
naji-alhusami <najihussami@gmail.com>
2023-03-10 01:55:26 +0300

    added courses without responsive

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,9 +1,34 @@
 import React from "react";
+import CoursesData from "./CoursesData";
 
 const Courses = () => {
+  const courses = CoursesData.map((course) => {
+    const courseDescription = course.description.split("||");
+
+    // if (course.id % 2) {
     return (
-        <h1>Courses</h1>
-    )
-}
+      <div
+        key={course.id}
+        className={`bg-white bg-opacity-95 w-9/12 mx-auto my-4 border-4 border-green-800 relative ${
+          course.id % 2 ? "border-l-0 float-left" : "border-r-0 float-right"
+        }`}
+      >
+        <h1 className="text-2xl font-bold lg:max-w-lg sm:max-w-md text-green-700 text-left m-4">
+          {course.name}
+        </h1>
+        <h2 className="absolute top-0 right-0 m-3 text-sm text-white bg-red-500 py-2 px-3">
+          Donwload Certificate
+        </h2>
+        <ul className="p-4 text-lg text-left">
+          {courseDescription.map((description) => (
+            <li key={description}>{description}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  });
+
+  return <div>{courses}</div>;
+};
 
 export default Courses;
--- src/components/Courses/CoursesData.js
+++ src/components/Courses/CoursesData.js
@@ -1,36 +1,37 @@
 const CoursesData = [
   {
-    id: "c1",
+    id: 1,
     name: "Git & GitHub - The Practical Guide",
-    description: "Finest fish and veggies",
+    description:
+      "Work with Git, including all about repositories, commits, branches || Merging, rebasing, the stash and learn how to manage merge conflicts || Collaborate in projects via Git & GitHub in teams & organizations || Contribute to open-source projects via forks & pull requests",
     price: 22.99,
   },
   {
-    id: "c2",
+    id: 2,
     name: "CSS - The Complete Guide 2023 (incl. Flexbox, Grid & Sass)",
     description: "Finest fish and veggies",
     price: 22.99,
   },
   {
-    id: "c3",
+    id: 3,
     name: "JavaScript - The Complete Guide 2023 (Beginner + Advanced)",
     description: "A german specialty!",
     price: 16.5,
   },
   {
-    id: "c4",
+    id: 4,
     name: "React - The Complete Guide (incl Hooks, React Router, Redux)",
     description: "American, raw, meaty",
     price: 12.99,
   },
   {
-    id: "c5",
+    id: 5,
     name: "Understanding TypeScript",
     description: "Healthy...and green...",
     price: 18.99,
   },
   {
-    id: "c6",
+    id: 6,
     name: "Gatsby.js Tutorial and Projects Course",
     description: "Healthy...and green...",
     price: 18.99,
