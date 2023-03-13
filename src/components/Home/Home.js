28468dceb4a279da84b36e0020ae7639a03b6b4c
naji-alhusami <najihussami@gmail.com>
2023-03-13 14:32:37 +0300

    more new design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,72 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Cover from "../Images/Cover.png";
 
 const Home = () => {
   return (
-    // <header className="mt-n20 pt-20 h-screen bg-primary-10 relative">
-    //   <section className="grid h-full items-center w-90vw mx-auto max-w-screen-lg">
-    //     <article className="bg-green-700">
-    //       <div>
-    //         <div className="underline"></div>
-    //         <h1>i'm john</h1>
-    //         <h4>freelance web and mobile UI/UX Designer</h4>
-    //         <button type="button" to="/contact" className="btn">
-    //           contact me
-    //         </button>
-    //       </div>
-    //     </article>
-    //     <img
-    //       src={Cover}
-    //       alt="portfolio"
-    //       className="hero-img"
-    //       placeholder="blurred"
-    //     />
-    //     {/* <img src={Cover} alt="portfolio" className="hero-img-svg" /> */}
-    //   </section>
-    // </header>
-    // <header className="absolute z-0 bg-gradient-to-r from-green-300 via-white mt-n20 w-screen h-screen ">
-    //   <section className="h-full items-center mx-auto w-full">
-    //     <div className="flex h-full">
-    //       <div className="h-full w-1/2 ">
-    //         <div>
-    //           <div className="underline"></div>
-    //           <h1>i'm john</h1>
-    //           <h4>freelance web and mobile UI/UX Designer</h4>
-    //           <button type="button" to="/contact" className="btn">
-    //             contact me
-    //           </button>
-    //         </div>
-    //       </div>
-    //       <div className="relative z-10 h-full">
-    //         <img
-    //           src={Cover}
-    //           alt="portfolio"
-    //           className=" h-full bg-transparent"
-    //           placeholder="blurred"
-    //         />
-    //       </div>
-    //     </div>
-    //     {/* <img src={Cover} alt="svg" className="svg-img" /> */}
-    //   </section>
-    // </header>
     <header className="absolute z-0 w-screen h-screen">
       <div className="bg-gradient-to-r from-green-300 via-white h-full">
         <div className="h-full items-center mx-auto max-w-screen-lg flex">
           <div className="w-1/2">
             <div>
-              <div className="underline"></div>
-              <h1>i'm john</h1>
-              <h4>freelance web and mobile UI/UX Designer</h4>
-              <button type="button" to="/contact" className="btn">
-                contact me
-              </button>
+              <div className="bg-emerald-500 py-1 w-24 my-4" alt="A line" />
+            </div>
+            <h1 className="text-6xl text-bold text-teal-800 text-left my-4">I'm Naji</h1>
+            <h4 className="text-2xl text-left my-2 text-bold">Font-End Development</h4>
+            <div className="text-xl text-bold float-left">
+              Please Check:
+              <Link to="/about">
+                <button
+                  type="button"
+                  className="text-xl bg-emerald-500 p-3 rounded-md m-3 hover:text-white hover:bg-emerald-700"
+                >
+                  About Me
+                </button>
+              </Link>
+              OR
+              <Link to="/contact">
+                <button
+                  type="button"
+                  className="text-xl bg-emerald-500 p-3 rounded-md m-3 hover:text-white hover:bg-emerald-700"
+                >
+                  Contact Me
+                </button>
+              </Link>
             </div>
           </div>
           <div className="relative z-10 my-4">
             <img
               src={Cover}
               alt="portfolio"
-              className="my-4"
+              className="m-10"
               placeholder="blurred"
             />
           </div>
