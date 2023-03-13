b17dacba7be1b08b1859716e4f452640bc870be6
naji-alhusami <najihussami@gmail.com>
2023-03-13 11:42:01 +0300

    new changes to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,43 +1,78 @@
-// import Card from "../Reusable/Card";
-// import MealItem from "./MealItem/MealItem";
-// import PersonalPhoto from "../Images/PersonalPhoto.jpg";
-import HomeAndSkillsCard from "../Cards/HomeAndSkillsCard";
-import { HiArrowNarrowRight } from "react-icons/hi";
-// import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React from "react";
+import Cover from "../Images/Cover.png";
 
 const Home = () => {
   return (
-    <div className="min-h-screen w-screen flex justify-center items-center">
-      <HomeAndSkillsCard>
-        <div className="p-4 ">
-          {/* <img
-            src={PersonalPhoto}
-            alt="my-pict"
-            className="w-60 h-64 my-4 mx-auto overflow-hidden rounded-full"
-          /> */}
-          <h1 className="text-4xl font-bold mb-4">NAJI ALHUSAMI</h1>
-          <h3 className="text-lg mb-4">Front-End Development</h3>
-          <p className="text-lg mb-4 lg:px-20">
-            I'm a Front-End Developer, specializing in building responsive web
-            applications.
-          </p>
-          <Link to="/about" className="flex justify-center">
-            <button className="m-4 px-8 py-4 bg-green-700 hover:bg-green-900 flex items-center rounded-md text-white transition duration-100 font-bold">
-              Please Read More About Me
-                <HiArrowNarrowRight className="ml-3 " />
-            </button>
-          </Link>
-          <strong>OR</strong>
-          <Link to="/about" className="flex justify-center">
-            <button className="m-4 px-8 py-4 bg-green-700 hover:bg-green-900 flex items-center rounded-md text-white transition duration-100 font-bold">
-              Please Read More About Me
-                <HiArrowNarrowRight className="ml-3 " />
-            </button>
-          </Link>
+    // <header className="mt-n20 pt-20 h-screen bg-primary-10 relative">
+    //   <section className="grid h-full items-center w-90vw mx-auto max-w-screen-lg">
+    //     <article className="bg-green-700">
+    //       <div>
+    //         <div className="underline"></div>
+    //         <h1>i'm john</h1>
+    //         <h4>freelance web and mobile UI/UX Designer</h4>
+    //         <button type="button" to="/contact" className="btn">
+    //           contact me
+    //         </button>
+    //       </div>
+    //     </article>
+    //     <img
+    //       src={Cover}
+    //       alt="portfolio"
+    //       className="hero-img"
+    //       placeholder="blurred"
+    //     />
+    //     {/* <img src={Cover} alt="portfolio" className="hero-img-svg" /> */}
+    //   </section>
+    // </header>
+    // <header className="absolute z-0 bg-gradient-to-r from-green-300 via-white mt-n20 w-screen h-screen ">
+    //   <section className="h-full items-center mx-auto w-full">
+    //     <div className="flex h-full">
+    //       <div className="h-full w-1/2 ">
+    //         <div>
+    //           <div className="underline"></div>
+    //           <h1>i'm john</h1>
+    //           <h4>freelance web and mobile UI/UX Designer</h4>
+    //           <button type="button" to="/contact" className="btn">
+    //             contact me
+    //           </button>
+    //         </div>
+    //       </div>
+    //       <div className="relative z-10 h-full">
+    //         <img
+    //           src={Cover}
+    //           alt="portfolio"
+    //           className=" h-full bg-transparent"
+    //           placeholder="blurred"
+    //         />
+    //       </div>
+    //     </div>
+    //     {/* <img src={Cover} alt="svg" className="svg-img" /> */}
+    //   </section>
+    // </header>
+    <header className="absolute z-0 w-screen h-screen">
+      <div className="bg-gradient-to-r from-green-300 via-white h-full">
+        <div className="h-full items-center mx-auto max-w-screen-lg flex">
+          <div className="w-1/2">
+            <div>
+              <div className="underline"></div>
+              <h1>i'm john</h1>
+              <h4>freelance web and mobile UI/UX Designer</h4>
+              <button type="button" to="/contact" className="btn">
+                contact me
+              </button>
+            </div>
+          </div>
+          <div className="relative z-10 my-4">
+            <img
+              src={Cover}
+              alt="portfolio"
+              className="my-4"
+              placeholder="blurred"
+            />
+          </div>
         </div>
-      </HomeAndSkillsCard>
-    </div>
+      </div>
+    </header>
   );
 };
 
