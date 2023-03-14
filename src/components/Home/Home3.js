f4c7aa6146a32addcc23f7b707ffcacf5eb879d7
naji-alhusami <najihussami@gmail.com>
2023-03-14 12:50:47 +0300

    added animition to the home page

--- src/components/Home/Home3.js
+++ src/components/Home/Home3.js
@@ -2,17 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Cover from "../Images/Cover.png";
 import SocialLinks from "./SocialLinks";
+import { motion } from "framer-motion";
 import "./Home3.css";
 
 const Home = () => {
   return (
     <header className="relative">
       <div className="relative flex flex-col md:flex-row md:items-center lg:ml-20 lg:mt-4">
-        <div className="flex-1 flex flex-col justify-center text-left md:pl-8 custom-mobile-styles">
+        <motion.div
+          initial={{ x: -400 }}
+          animate={{ x: 0 }}
+          transition={{ delay: 0.2 }}
+          className="flex-1 flex flex-col justify-center text-left md:pl-8 custom-mobile-styles"
+        >
           <div className="bg-emerald-500 h-1 w-28 mb-6" alt="A line" />
-          <h1 className="text-7xl font-bold text-teal-900 mb-4">I'm Naji</h1>
-          <h3 className="text-2xl font-bold mb-6">Font-End Development</h3>
-          <div className="text-xl font-bold mb-8">
+          <motion.h1
+            initial={{ x: -400 }}
+            animate={{ x: 0 }}
+            transition={{ delay: 0.4 }}
+            className="text-7xl font-bold text-teal-900 mb-4"
+          >
+            I'm Naji
+          </motion.h1>
+          <motion.h3
+            initial={{ x: -400 }}
+            animate={{ x: 0 }}
+            transition={{ delay: 0.6 }}
+            className="text-2xl font-bold mb-6"
+          >
+            Font-End Development
+          </motion.h3>
+          <motion.div
+            initial={{ x: -400 }}
+            animate={{ x: 0 }}
+            transition={{ delay: 0.8 }}
+            className="text-xl font-bold mb-8"
+          >
             <Link to="/about">
               <button
                 type="button"
@@ -30,8 +55,13 @@ const Home = () => {
                 Contact Me
               </button>
             </Link>
-          </div>
-          <div className="flex">
+          </motion.div>
+          <motion.div
+            initial={{ x: -400 }}
+            animate={{ x: 0 }}
+            transition={{ delay: 1 }}
+            className="flex"
+          >
             {SocialLinks.map((link) => {
               return (
                 <a
@@ -43,16 +73,21 @@ const Home = () => {
                 </a>
               );
             })}
-          </div>
-        </div>
-        <div className="flex lg:mt-20 lg:mr-56 md:mr-32 md:mt-20 hidden sm:block">
+          </motion.div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.2 }}
+          className="flex lg:mt-20 lg:mr-56 md:mr-32 md:mt-20 hidden sm:block"
+        >
           <img
             src={Cover}
             alt="portfolio"
             className="object-cover h-full w-full"
             placeholder="blurred"
           />
-        </div>
+        </motion.div>
       </div>
     </header>
   );
