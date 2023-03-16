5b5ababede9e6def6a5c71f9ad75d6aaeb56d449
naji-alhusami <najihussami@gmail.com>
2023-03-16 12:34:23 +0300

    added some responsive to Home page

--- src/components/Home/Home3.js
+++ src/components/Home/Home3.js
@@ -3,21 +3,25 @@ import { Link } from "react-router-dom";
 import Cover from "../Images/Cover.png";
 import SocialLinks from "./SocialLinks";
 import { motion } from "framer-motion";
+// import { HiOutlineMail } from "react-icons/hi";
+// import { BsFillPersonLinesFill } from "react-icons/bs";
+// import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./Home3.css";
 
 const Home = () => {
   return (
     <header className=" relative">
-      <div className="relative flex flex-col md:flex-row md:items-center lg:ml-20 lg:mt-2">
+      {/* <div className="relative flex flex-col md:flex-row md:items-center lg:ml-16 mt-2"> */}
+      <div className="relative flex xl:flex-row xl:mt-2 lg:flex-row lg:mt-2 md:flex-row md:mt-10 md:items-center lg:ml-16 mt-2 image-styles-s">
         <motion.div
-          initial={{ x: -400 }}
+          initial={{ x: -600 }}
           animate={{ x: 0 }}
           transition={{ delay: 0.2 }}
-          className="flex-1 flex flex-col justify-center text-left md:pl-8 custom-mobile-styles"
+          className="flex-1 flex flex-col justify-center text-left md:mb-8 md:ml-20 sm:mt-20 sm:ml-24 div-styles-xs"
         >
           <div className="bg-emerald-500 h-1 w-28 mb-6" alt="A line" />
           <motion.h1
-            initial={{ x: -400 }}
+            initial={{ x: -600 }}
             animate={{ x: 0 }}
             transition={{ delay: 0.4 }}
             className="text-7xl font-bold text-teal-900 mb-4"
@@ -25,7 +29,7 @@ const Home = () => {
             I'm Naji
           </motion.h1>
           <motion.h3
-            initial={{ x: -400 }}
+            initial={{ x: -600 }}
             animate={{ x: 0 }}
             transition={{ delay: 0.6 }}
             className="text-2xl font-bold mb-6"
@@ -33,7 +37,7 @@ const Home = () => {
             Font-End Development
           </motion.h3>
           <motion.div
-            initial={{ x: -400 }}
+            initial={{ x: -600 }}
             animate={{ x: 0 }}
             transition={{ delay: 0.8 }}
             className="text-xl font-bold mb-8"
@@ -79,7 +83,8 @@ const Home = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.2 }}
-          className="flex lg:mt-20 lg:mr-56 md:mr-32 md:mt-20 hidden sm:block"
+          // className="flex mr-20 lg:mt-20 lg:mr-56 md:mr-32 md:mt-20 hidden sm:block "
+          className="flex xl:mr-56 xl:mt-20 xl:block lg:mr-16 lg:mt-20 lg:block md:mr-10 md:mt-10 md:block image-styles-s sm:hidden hidden"
         >
           <img
             src={Cover}
@@ -89,6 +94,47 @@ const Home = () => {
           />
         </motion.div>
       </div>
+      {/* <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-blue-600">
+            <a
+              href="https://www.linkedin.com/in/omar-bassatni-40a762188/"
+              target="_blank"
+              rel="noreferrer"
+              className="flex justify-between items-center w-full"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              href="https://github.com/OmarBassatni97"
+              target="_blank"
+              rel="noreferrer"
+              className="flex justify-between items-center w-full"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              href="mailto:omarbassatni@gmail.com"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              href="www.google.com"
+              download
+              className="flex justify-between items-center w-full"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div> */}
     </header>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,8 +6,7 @@ import {
   FaTimes,
   //  FaGithub, FaLinkedin
 } from "react-icons/fa";
-// import { HiOutlineMail } from "react-icons/hi";
-// import { BsFillPersonLinesFill } from "react-icons/bs";
+
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
@@ -15,151 +14,153 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
 
   return (
-      <nav className="relative w-full h-[60px] flex justify-between items-center px-10 z-2 ">
-        <motion.div
-          initial={{
-            opacity: 0,
-            x: -500,
-          }}
-          animate={{
-            opacity: 1,
-            x: 0,
-          }}
-          transition={{ duration: 2 }}
-        >
-          <Link to="/">
-            <img src={Logo} alt="logo" className="w-[60px]" />
-          </Link>
-        </motion.div>
-        {/* menu */}
-        <motion.ul
-          className="hidden md:flex text-lg"
-          initial={{
-            opacity: 0,
-            x: 500,
-          }}
-          animate={{
-            opacity: 1,
-            x: 0,
-          }}
-          transition={{ duration: 2 }}
-        >
-          <Link to="/">
-            <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
-              Home
-            </li>
-          </Link>
-          <Link to="/about">
-            <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
-              About
-            </li>
-          </Link>
-          <Link to="/skills">
-            <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
-              Skills
-            </li>
-          </Link>
-          <Link to="/projects">
-            <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
-              Projects
-            </li>
-          </Link>
-          <Link to="/courses">
-            <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
-              Courses
-            </li>
-          </Link>
-          <Link to="/contact">
-            <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-100 font-bold">
-              Contact
-            </li>
-          </Link>
-        </motion.ul>
-        <div
-          className="md:hidden z-10"
+    <nav className="relative w-full h-[60px] flex justify-between items-center lg:px-0 px-[5rem]  z-2 ">
+      <motion.div
+      className="lg:px-[5rem] md:px-12 sm:pl-4"
+        initial={{
+          opacity: 0,
+          x: -500,
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{ duration: 2 }}
+      >
+        <Link to="/">
+          <img src={Logo} alt="logo" className="w-[60px]" />
+        </Link>
+      </motion.div>
+      {/* menu */}
+      <motion.ul
+        className="hidden md:flex text-lg"
+        initial={{
+          opacity: 0,
+          x: 500,
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{ duration: 2 }}
+      >
+        <Link to="/">
+          <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
+            Home
+          </li>
+        </Link>
+        <Link to="/about">
+          <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
+            About
+          </li>
+        </Link>
+        <Link to="/skills">
+          <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
+            Skills
+          </li>
+        </Link>
+        <Link to="/projects">
+          <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
+            Projects
+          </li>
+        </Link>
+        <Link to="/courses">
+          <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-300 font-bold">
+            Courses
+          </li>
+        </Link>
+        <Link to="/contact">
+          <li className="mx-1 p-3 hover:bg-emerald-500 hover:rounded-md hover:text-white transition duration-100 font-bold">
+            Contact
+          </li>
+        </Link>
+      </motion.ul>
+      <div
+        className="md:hidden z-10"
+        onClick={() => {
+          setNav(!nav);
+        }}
+      >
+        {nav ? <FaTimes /> : <FaBars />}
+      </div>
+      {/* mobile menu */}
+      <motion.ul
+        className={`${
+          nav ? "absolute" : "hidden"
+        }  top-0 left-0 w-screen h-screen flex flex-col justify-center items-center bg-white `}
+        initial={{
+          opacity: 0,
+          x: 0,
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{ duration: 2 }}
+      >
+        <Link
           onClick={() => {
             setNav(!nav);
           }}
+          to="/"
         >
-          {nav ? <FaTimes /> : <FaBars />}
-        </div>
-        {/* mobile menu */}
-        <motion.ul
-          className={`${
-            nav ? "absolute" : "hidden"
-          }  top-0 left-0 w-screen h-screen flex flex-col justify-center items-center bg-white `}
-          initial={{
-            opacity: 0,
-            x: 0,
+          <li className="p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
+            Home
+          </li>
+        </Link>
+        <Link
+          onClick={() => {
+            setNav(!nav);
           }}
-          animate={{
-            opacity: 1,
-            x: 0,
-          }}
-          transition={{ duration: 2 }}
+          to="/about"
         >
-          <Link
-            onClick={() => {
-              setNav(!nav);
-            }}
-            to="/"
-          >
-            <li className="p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
-              Home
-            </li>
-          </Link>
-          <Link
-            onClick={() => {
-              setNav(!nav);
-            }}
-            to="/about"
-          >
-            <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
-              About
-            </li>
-          </Link>
-          <Link
-            onClick={() => {
-              setNav(!nav);
-            }}
-            to="/skills"
-          >
-            <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
-              Skills
-            </li>
-          </Link>
-          <Link
-            onClick={() => {
-              setNav(!nav);
-            }}
-            to="/projects"
-          >
-            <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
-              Projects
-            </li>
-          </Link>
-          <Link
-            onClick={() => {
-              setNav(!nav);
-            }}
-            to="/courses"
-          >
-            <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
-              Courses
-            </li>
-          </Link>
-          <Link
-            onClick={() => {
-              setNav(!nav);
-            }}
-            to="/contact"
-          >
-            <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
-              Contact
-            </li>
-          </Link>
-        </motion.ul>
-      </nav>
+          <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
+            About
+          </li>
+        </Link>
+        <Link
+          onClick={() => {
+            setNav(!nav);
+          }}
+          to="/skills"
+        >
+          <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
+            Skills
+          </li>
+        </Link>
+        <Link
+          onClick={() => {
+            setNav(!nav);
+          }}
+          to="/projects"
+        >
+          <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
+            Projects
+          </li>
+        </Link>
+        <Link
+          onClick={() => {
+            setNav(!nav);
+          }}
+          to="/courses"
+        >
+          <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
+            Courses
+          </li>
+        </Link>
+        <Link
+          onClick={() => {
+            setNav(!nav);
+          }}
+          to="/contact"
+        >
+          <li className="my-3 p-3 text-4xl transition duration-300 hover:bg-green-700 hover:rounded-md hover:text-white transition duration-300">
+            Contact
+          </li>
+        </Link>
+      </motion.ul>
+      
+    </nav>
   );
 };
 
