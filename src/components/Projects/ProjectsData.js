ce053d1e8497d5fa32ae1ad74b7f614c62170d50
naji-alhusami <najihussami@gmail.com>
2023-05-21 13:11:44 +0300

    updated the projects data

--- src/components/Projects/ProjectsData.js
+++ src/components/Projects/ProjectsData.js
@@ -13,28 +13,29 @@ const ProjectsData = [
       "Online Therapist project allows users to signup/login to take an appointment with a therapist and buy therapy sessions. ",
     image: Therapist,
     info: "- Worked with Figma design tools to translate design concepts into functional user interfaces. || - Member of the Re-coded graduation project team, specifically responsible for the Redux toolkit implementation. || - Led the team for whole project duration. || - Conducted daily meetings with team members to discuss progress and coordinate efforts. || - Facilitated weekly meetings with our senior developers in Re-Coded to seek guidance. || - Took charge of the presentation for our team project, showcasing the key features, functionalities to our trainers.",
-    code: "https://github.com/Recoded-Spark-Turkey-2022/Code-Wars-Capstone",
+    code: "https://github.com/naji-alhusami/Online-Therapist",
     demo: "https://code-wars.netlify.app/",
   },
-  // {
-    // id: 2,
-    // name: "Health Centers",
-    // technologies:
-    // "HTML, TailwindCSS, Reactjs, Redux toolkit, Firebase, Nominatim API (OpenStreetMap).",
-    // description: "This project allows users to signup/login using Email and Gmail, the user can add Health Centers to the map and remove it.",
-    // image: Therapist,
-    // info: "I built this website on my own, I learned a lot of things like reactjs, reusable components, Firbase (By adding data to Realtime Database and fetching them to my project), In addition, I learned how to send data to Firebase.",
-    // code: "https://github.com/naji-alhusami/health-center-app",
-    // demo: "https://health-centers-app.netlify.app/",
-  // },
+  {
+    id: 2,
+    name: "Health Centers",
+    technologies:
+      "HTML, TailwindCSS, Reactjs, Redux toolkit, Firebase, Leaflet Library.",
+    description:
+      "The Health Centers Application enables users to conveniently sign up or log in using their email or Google accounts. It offers the ability to add or remove health centers to a map and table. Additionally, the map showcasing all the health centers is accessible to the public without requiring a login.",
+    image: Therapist,
+    info: "- Applied the insights and guidance from the Firebase documentation. || - Implemented Leaflet, an interactive mapping library, to deliver dynamic and customizable map functionalities. || - The Health Centers Application showcases my expertise in creating intuitive and interactive interfaces while addressing the specific needs of users in the healthcare sector.",
+    code: "https://github.com/naji-alhusami/health-center-app",
+    demo: "https://health-centers-app.netlify.app/",
+  },
   {
     id: 3,
     name: "Online Food Order",
     technologies: "HTML, CSS, Reactjs, Firebase.",
     description:
-      "This project allows us to order food online after choosing the restaurants, it allows us to add the quantity ofeach item to the final basket. In addition it allows us to send our order to the backend (Firebase). ",
+      "The online food ordering application is designed to streamline the process of ordering food through a website. Users can select the desire items from the menu, add them to the cart,  proceed to place an order and pay with card.",
     image: Foodorder,
-    info: "I built this website on my own, I learned a lot of things like reactjs, reusable components, Firbase (By adding data to Realtime Database and fetching them to my project), In addition, I learned how to send data to Firebase.",
+    info: "- Conducted thorough testing and debugging to deliver a stable and error-free application. || - Demonstrated strong problem-solving skills. || - Developed the application from scratch as a sole contributor, handling all aspects of the single project.",
     code: "https://github.com/naji-alhusami/food-order-project",
     demo: "https://online-food-order-reactjs.netlify.app",
   },
